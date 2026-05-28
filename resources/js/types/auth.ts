ca8bd964e24a3d77ceccc104d3f88b9201084bc0
phar-dev <pablo.hv.2002@gmail.com>
2026-05-27 18:20:37 -0600

    feat: add roles field to User TypeScript type

--- resources/js/types/auth.ts
+++ resources/js/types/auth.ts
@@ -2,8 +2,9 @@ export type User = {
     id: number;
     name: string;
     email: string;
-    avatar?: string;
-    email_verified_at: string | null;
+  avatar?: string;
+  roles?: Role[];
+  email_verified_at: string | null;
     two_factor_enabled?: boolean;
     created_at: string;
     updated_at: string;
